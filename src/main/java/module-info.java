6845c23e80b8907352edd717e2b833d75bdf5a14
alexgonzalez001 <alexgr20063@gmail.com>
2026-06-03 19:51:18 +0200

    Añadida la conexion SQLite y las dependencias en pom.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
-module com.example.proyecto_zoo_1dam {
+module org.proyecto_zoo_1dam {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
 
 
     opens org.proyecto_zoo_1dam to javafx.fxml;
