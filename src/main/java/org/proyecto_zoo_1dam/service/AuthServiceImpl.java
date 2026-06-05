a4bd30af88568b0bf74697132222384f52f0467f
EduardoLC17 <eduardo.lopezcozar@gmail.com>
2026-06-05 20:35:47 +0200

    Creacion de servicios de autenticacion

--- src/main/java/org/proyecto_zoo_1dam/service/AuthServiceImpl.java
+++ src/main/java/org/proyecto_zoo_1dam/service/AuthServiceImpl.java
@@ -1,9 +1,20 @@
 package org.proyecto_zoo_1dam.service;
 
+import org.proyecto_zoo_1dam.dao.UserDao;
+import org.proyecto_zoo_1dam.dao.UserDaoImpl;
+
 public class AuthServiceImpl implements AuthService {
 
-    @Override
+    private final UserDao userDao = new UserDaoImpl();
+
     public boolean login(String username, String password) {
-        return "zoo".equals(username) && "zoo".equals(password);
+
+        if (username == null || username.isBlank() || password == null || password.isBlank()) {
+            return false;
+        }
+
+        return userDao.findByUsername(username)
+                .map(u -> u.getPassword().equals(password))
+                .orElse(false);
     }
 }
