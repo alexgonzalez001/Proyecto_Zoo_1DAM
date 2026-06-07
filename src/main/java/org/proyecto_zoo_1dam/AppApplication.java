9c936ceca412f069a73333dd2c731fbee377281f
davidmoral22 <davidml0612@gmail.com>
2026-06-07 21:27:02 +0200

    Cntrar ventana y fijar tamaño más solución de la advertencia SLF4J

--- src/main/java/org/proyecto_zoo_1dam/AppApplication.java
+++ src/main/java/org/proyecto_zoo_1dam/AppApplication.java
@@ -3,6 +3,7 @@ package org.proyecto_zoo_1dam;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class AppApplication extends Application {
@@ -13,6 +14,8 @@ public class AppApplication extends Application {
                 ("/org/proyecto_zoo_1dam/proyecto2/fxml/login.fxml")));
         stage.setTitle("Zoo - Login");
         stage.setScene(scene);
+        stage.setResizable(false); // Bloquea el tamaño para que nadie rompa el diseño estirando la ventana
+        stage.centerOnScreen();    // Hace que aparezca justo en el centro de la pantalla del monitor
         stage.show();
     }
 
